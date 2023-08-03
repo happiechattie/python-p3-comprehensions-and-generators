959f55b7af15e8fa6254e6f83789858ce5da9210
Christine Baik <christuaik@gmail.com>
2023-08-03 10:02:42 -0700

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_list = [item for item in num_list if item % 2 == False]
+    return new_list
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [item + "!" for item in sentence_list if True]
+    return new_list
